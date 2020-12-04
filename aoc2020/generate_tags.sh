fe99bbe0d92e140284cd90a995f7cd2568a0f87f
Will R <email@astutecat.dev>
2020-12-04 14:11:02 +0100

    generate tags file on commit

--- aoc2020/generate_tags.sh
+++ aoc2020/generate_tags.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 ERL_DIR=$(asdf where erlang)
 [ ! -f erlang.TAGS ] && ctags --recurse -e -f erlang.TAGS $ERL_DIR/lib
 ctags --recurse -e -f app.TAGS src test
---
+++ aoc2020/test/aoc2020_day1_tests.erl
@@ -0,0 +1,3 @@
+-module(aoc2020_day1_tests).
+
+-include_lib("eunit/include/eunit.hrl").
