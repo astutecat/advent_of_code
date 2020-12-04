9199eb773bb6769af91347984555c398ba45caf9
Will R <email@astutecat.dev>
2020-12-04 15:21:23 +0100

    Day one complete.

--- aoc2020/src/aoc2020_day1.erl
+++ aoc2020/src/aoc2020_day1.erl
@@ -1,7 +1,7 @@
 -module(aoc2020_day1).
 
 %% -compile(export_all).
--export([problem_1/0]).
+-export([problem_1/0, problem_2/0]).
 
 problem_1() ->
     Lines = readlines("src/aoc2020_day1_input.txt"),
@@ -11,8 +11,17 @@ problem_1() ->
                end,
     FilterFun = fun(Val) -> is_number(Val) end,
     Ints = lists:filter(FilterFun, lists:map(ParseFun, Lines)),
-    SortedInts = lists:sort(fun(A, B) -> A > B end, Ints),
-    SortedInts.
+    hd([{X, Y} || X <- Ints, Y <- Ints, X + Y == 2020]).
+
+problem_2() ->
+    Lines = readlines("src/aoc2020_day1_input.txt"),
+    ParseFun = fun(Line) -> 
+                       {I, _} = string:to_integer(Line),
+                       I
+               end,
+    FilterFun = fun(Val) -> is_number(Val) end,
+    Ints = lists:filter(FilterFun, lists:map(ParseFun, Lines)),
+    hd([{X, Y, Z} || X <- Ints, Y <- Ints, Z <- Ints, X + Y + Z == 2020]).
 
 readlines(FileName) ->
     {ok, Data} = file:read_file(FileName),
--- aoc2020/test/aoc2020_day1_tests.erl
+++ aoc2020/test/aoc2020_day1_tests.erl
@@ -1,3 +1,13 @@
 -module(aoc2020_day1_tests).
 
 -include_lib("eunit/include/eunit.hrl").
+
+problem_1_test() ->
+    {A, B} = aoc2020_day1:problem_1(),
+    ?assertEqual(2020, A + B),
+    ?debugFmt("Part one result: ~p~n", [A * B]).
+
+problem_2_test() ->
+    {A, B, C} = aoc2020_day1:problem_2(),
+    ?assertEqual(2020, A + B + C),
+    ?debugFmt("Part one result: ~p~n", [A * B * C]).
