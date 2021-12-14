f39d8d16c47486527429230d07eadada11b24cb5
AstuteCat <github@astutecat.dev>
2021-12-14 09:35:22 +0100

    more day1 progress sheesh

--- aoc2021/day01b/src/main.rs
+++ aoc2021/day01b/src/main.rs
@@ -14,37 +14,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let reader = BufReader::new(file);
 
-     for line in reader.lines() {
-         if let Ok(l) = line {
-             let parsed = l.parse::<i32>()?;
-             if window_a.len() < 3 {
-                 println!("window_a length is {}, pushing {}", window_a.len(), parsed);
-                 window_a.push(parsed)
-             } else if window_b.len() < 3 {
-                 println!("window_b length is {}, pushing {}", window_b.len(), parsed);
-                 window_b.push(parsed)
-             } 
-
-             if window_a.len() == 3 && window_b.len() == 3 {
-                 println!("window a and b length is 3");
-                 println!("Summing window a: {}", window_a.join(","));
-                 let a_sum: i32 = window_a.iter().sum();
-                 println!("Summing window b: {}", window_b.join(","));
-                 let b_sum: i32 = window_b.iter().sum();
-                 if a_sum < b_sum {
-                     num_increased += 1;
-                 } else if a_sum > b_sum {
-                     num_decreased += 1;
-                 } else if a_sum == b_sum {
-                     no_change += 1;
-                 }
-
-                 window_a = window_a.clone();
-                 window_b.clear();
-             }
-         }
-     }
-    
+    let lines:Vec<i32> = reader.lines().map(|x| x.unwrap().parse::<i32>().unwrap()).collect();
 
     println!(
         "decreased: {}, increased: {}, no change: {}",
