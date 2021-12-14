09d133dd74be6dcdcf88958db8d753cf559d124c
Will Rogers <email@willrogers.me.uk>
2021-12-14 09:47:48 +0100

    day1 done?

--- aoc2021/day01a/target/debug/day01a.d
+++
@@ -1,1 +0,0 @@
-/Users/astutecat/repos/advent_of_code/aoc2021/day01a/target/debug/day01a: /Users/astutecat/repos/advent_of_code/aoc2021/day01a/src/main.rs
--- aoc2021/day01b/src/main.rs
+++ aoc2021/day01b/src/main.rs
@@ -4,8 +4,7 @@ use std::{error::Error, i32};
 use std::fs::File;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut window_a = Vec::new();
-    let mut window_b = Vec::new();
+    let mut prev: Option<i32> = None;
     let mut num_increased = 0;
     let mut num_decreased = 0;
     let mut no_change = 0;
@@ -14,7 +13,21 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let reader = BufReader::new(file);
 
-    let lines:Vec<i32> = reader.lines().map(|x| x.unwrap().parse::<i32>().unwrap()).collect();
+    let parsed:Vec<i32> = reader.lines().map(|x| x.unwrap().parse::<i32>().unwrap()).collect();
+
+    for i in parsed.windows(3) {
+      let curr = i.iter().sum::<i32>();
+      if let Some(p) = prev {
+        if curr > p {
+          num_increased += 1;
+        } else if curr < p {
+          num_decreased += 1;
+        } else {
+          no_change += 1;
+        }
+      }
+      prev = Some(curr);
+    }
 
     println!(
         "decreased: {}, increased: {}, no change: {}",
