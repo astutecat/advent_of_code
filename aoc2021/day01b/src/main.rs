f8644fb035f44c941522820cf3ae5405fbf152de
Will Rogers <email@willrogers.me.uk>
2021-12-14 09:53:55 +0100

    day1

--- aoc2021/day01b/src/main.rs
+++ aoc2021/day01b/src/main.rs
@@ -1,38 +1,26 @@
-use std::io::{BufRead, BufReader};
-use std::{error::Error, i32};
+use std::error::Error;
 
-use std::fs::File;
+use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut prev: Option<i32> = None;
+    let mut prev: Option<u32> = None;
     let mut num_increased = 0;
-    let mut num_decreased = 0;
-    let mut no_change = 0;
 
-    let file = File::open("input.txt")?;
+    let data = fs::read_to_string("input.txt").expect("unable to read file");
 
-    let reader = BufReader::new(file);
-
-    let parsed:Vec<i32> = reader.lines().map(|x| x.unwrap().parse::<i32>().unwrap()).collect();
+    let parsed:Vec<u32> = data.lines().map(|x| x.parse::<u32>().unwrap()).collect();
 
     for i in parsed.windows(3) {
-      let curr = i.iter().sum::<i32>();
+      let curr = i.iter().sum::<u32>();
       if let Some(p) = prev {
         if curr > p {
           num_increased += 1;
-        } else if curr < p {
-          num_decreased += 1;
-        } else {
-          no_change += 1;
-        }
+        }      
       }
       prev = Some(curr);
     }
 
-    println!(
-        "decreased: {}, increased: {}, no change: {}",
-        num_increased, num_decreased, no_change
-    );
+    println!("increased: {}", num_increased);
     Ok(())
 }
 
